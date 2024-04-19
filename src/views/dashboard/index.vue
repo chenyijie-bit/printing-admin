01d6e297fb63e5c8587e6837482fa92e9e2db484
v_陈亿杰1 <184913704@qq.com>
2024-04-19 17:32:18 +0800

    init

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import { ref } from "vue"
+const name = ref<string>("")
+function setName(value: string) {
+  console.log(name.value)
+  name.value = value
+}
+</script>
+
+<template>
+  <div style="padding-top: 200px">
+    <div>{{ name }}</div>
+    <el-input v-model="name" @input="(value) => setName(value)" />
+  </div>
+</template>
